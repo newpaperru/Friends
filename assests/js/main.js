ab1dca9b4ac03be7d7542cff624ac9811dbec296
newpaperru <104895604+newpaperru@users.noreply.github.com>
2022-06-22 23:07:30 +0300

    Add files via upload

---
+++ assests/js/main.js
@@ -0,0 +1,33 @@
+$(".gallery-list").magnificPopup ({
+	delegate: "a",
+	type: "image",
+	gallery: {
+		enabled: true
+	}
+});
+
+$(".main_page_rightSide-content-photo").magnificPopup ({
+	delegate: "a",
+	type: "image",
+	gallery: {
+		enabled: true
+	}
+});
+
+$(".main_page_rightSide-content-wall_12-img").magnificPopup ({
+	delegate: "a",
+	type: "image",
+	gallery: {
+		enabled: true
+	}
+});
+
+$(".main_page_rightSide-contents-gallery-item").magnificPopup ({
+	delegate: "a",
+	type: "image",
+	gallery: {
+		enabled: true
+	}
+});
+
+
