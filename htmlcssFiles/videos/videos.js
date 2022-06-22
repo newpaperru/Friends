c526d0b0fe30e9558e0a13c79e9bb62fca9c098c
newpaperru <104895604+newpaperru@users.noreply.github.com>
2022-06-22 23:05:16 +0300

    Delete htmlcssFiles directory

--- htmlcssFiles/videos/videos.js
+++
@@ -1,8 +0,0 @@
-var elem = document.getElementById('player');
-if (elem.requestFullscreen) {
- elem.requestFullscreen();
-} else if (elem.mozRequestFullScreen) {
- elem.mozRequestFullScreen();
-} else if (elem.webkitRequestFullscreen) {
- elem.webkitRequestFullscreen();
-} ;
